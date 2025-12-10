4946036fd0076428d10390ea36316aecdd7df4a6
Davy <davy@datalink.be>
2025-12-10 11:11:13 +0100

    ♻️ refactor: add href prop to appFormButton
    => improve applyTheme method to handle full styling of the button
    => conditionally render <button> or <a> tag depending on :href input

--- .vitepress/theme/components/AppFormButton.vue
+++ .vitepress/theme/components/AppFormButton.vue
@@ -1,26 +1,38 @@
 <template>
-    <button class="px-4 py-2  border-b-4 rounded-md transition duration-300" :class="applyTheme()">
+    <button v-if="props.href === null" :class="applyTheme()">
         <slot/>
     </button>
+    <a v-else-if="props.href !== null" :href="props.href" :class="applyTheme()">
+        <slot/>
+    </a>
 </template>
 <script setup lang="ts">
     const props = withDefaults(
         defineProps<{
             theme?: "primary" | "secondary"
+            href?: string | null
         }>(), {
-            theme: "primary"
+            theme: "primary",
+            href: null
         }
     )
 
     const applyTheme = () => {
+        const style = [];
+
+        style.push('px-4 py-2 text-center border-b-4 rounded-md transition duration-300');
 
         switch(props.theme) {
             case "primary": {
-                return "bg-red-600 border-red-700 text-red-50 hover:bg-red-500";
+                style.push("bg-red-600 border-red-700 text-red-50 hover:bg-red-500");
+                break;
             }
             case "secondary": {
-                return "bg-neutral-300 border-neutral-400 text-neutral-700 hover:bg-neutral-200";
+                style.push("bg-neutral-300 border-neutral-400 text-neutral-700 hover:bg-neutral-200");
+                break;
             }
         }
+
+        return style.join(" ");
     }
 </script>
