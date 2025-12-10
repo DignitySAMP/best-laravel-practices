b097fb550cfd0190453acfe12a993e851861a3ac
Davy <davy@datalink.be>
2025-12-10 08:34:41 +0100

    ✨ feat: set up custom Layout.vue
    => according to documentation

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Custom Layout!</h1>
+  
+    <div v-if="page.isNotFound">
+        Custom 404 page!
+    </div>
+    <Content v-else />
+</template>
+<script setup lang="ts">
+import { useData, Content } from 'vitepress'
+const { page } = useData()
+</script>
---
+++ .vitepress/theme/index.ts
@@ -0,0 +1,12 @@
+
+// You can directly import Vue files in the theme entry
+// VitePress is pre-configured with @vitejs/plugin-vue.
+import { Theme } from 'vitepress'
+import Layout from './Layout.vue'
+
+export default {
+    Layout,
+    enhanceApp({ app, router, siteData }) {
+        // ...
+    }
+} satisfies Theme
