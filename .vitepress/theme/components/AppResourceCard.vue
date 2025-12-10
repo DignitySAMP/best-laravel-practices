10c54ef98ef8307ac84d5e81ecb0814451694f8f
Davy <davy@datalink.be>
2025-12-10 14:53:17 +0100

    ♻️ refactor(AppResourceCard): improve html semantics

--- .vitepress/theme/components/AppResourceCard.vue
+++ .vitepress/theme/components/AppResourceCard.vue
@@ -1,12 +1,12 @@
 <template>
-    <article class="pb-4 w-full flex items-center justify-between">
-        <div class="flex flex-col min-w-64">
+    <section class="pb-4 w-full flex items-center justify-between">
+        <aside class="flex flex-col min-w-64">
             <span class="text-xs text-gray-500">Added by</span>
             <span class="font-bold text-red-400">{{ post.frontmatter.author }}</span>
             <span class="text-sm text-gray-500">{{ new Date(post.frontmatter.date).toDateString() }}</span>
-        </div>
+        </aside>
 
-        <div class="w-full grid grid-cols-[1fr_auto] items-center text-gray-600">
+        <article class="w-full grid grid-cols-[1fr_auto] items-center text-gray-600">
             <div class="flex flex-col">
                 <span class="font-bold">{{ post.frontmatter.title }}</span>
                 <span>{{ post.frontmatter.description }}</span>
@@ -21,8 +21,8 @@
                     Read Full Article
                 </span>
             </a>
-        </div>
-    </article>
+        </article>
+    </section>
 </template>
 <script setup lang="ts">
 import { type ContentData } from 'vitepress'
