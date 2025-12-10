4d4862a4c9c2d116b9e3089b67ded9d3490881fc
rusu <rzrusu03@gmail.com>
2025-12-10 23:12:39 +0200

    ♻️ refactor(AppIndexHero): adjust layout and improve text styling for better readability

--- .vitepress/theme/components/AppIndexHero.vue
+++ .vitepress/theme/components/AppIndexHero.vue
@@ -2,21 +2,23 @@
 
     <div class="mt-32 grid grid-cols-2 gap-32">
 
-        <article class="flex flex-col gap-4">
+        <article class="flex flex-col gap-12">
 
-            <h1 class="text-7xl font-medium">
-                Write better <span class="text-laravel font-bold">Laravel</span> code
-            </h1>
-            <span class="text-2xl text-gray-500">
-                Browse a complete library of opinionated Laravel best practices, expert tips & tricks and battle-tested code patterns to level up your development skills fast.
-            </span>
-            <div class="w-full grid grid-cols-2 gap-6">
+            <div class="flex flex-col gap-4">
+                <h1 class="text-7xl font-medium">
+                    Write better <span class="text-laravel font-bold">Laravel</span> code
+                </h1>
+                <span class="text-lg text-gray-500">
+                    Browse a complete library of opinionated Laravel best practices, expert tips & tricks and battle-tested code patterns to level up your development skills fast.
+                </span>
+            </div>
+            <div class="w-full space-x-4">
 
-                <AppFormButton href="/resources">
+                <AppFormButton href="/resources" class="w-fit">
                     Browse Library
                 </AppFormButton>
 
-                <AppFormButton theme="secondary">
+                <AppFormButton theme="secondary" class="w-fit">
                     Contribute
                 </AppFormButton>
             </div>
