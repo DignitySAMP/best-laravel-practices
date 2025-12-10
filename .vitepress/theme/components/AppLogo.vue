e0655e27d33c2f58e6eaa46dc710aa8985dad973
rusu <rzrusu03@gmail.com>
2025-12-10 23:19:46 +0200

    ♻️ refactor(AppLayoutHeader, AppLogo): update header structure and logo styling

--- .vitepress/theme/components/AppLogo.vue
+++ .vitepress/theme/components/AppLogo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="size-10 bg-laravel text-white p-1.5 rounded-lg">
+    <div class="min-width-4 min-height-4 bg-laravel text-white p-1.5 rounded-lg">
         <IconLaravel/>
     </div>
 </template>
