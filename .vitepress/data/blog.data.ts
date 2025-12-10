aa382b7319c310c4f2ac5df749de7c97d12c3ac3
Davy <davy@datalink.be>
2025-12-10 09:09:08 +0100

    🔧 chore: extend data loader to add article sorting by date

--- .vitepress/data/blog.data.ts
+++ .vitepress/data/blog.data.ts
@@ -4,4 +4,13 @@ import { createContentLoader } from 'vitepress'
 declare const data: ContentData[]
 export { data }
 
-export default createContentLoader('blog/*.md')
+export default createContentLoader('blog/*.md', {
+    transform: (data) => {
+        return data.sort((a, b) => {
+            return (
+                new Date(b.frontmatter.date).getTime()
+                - new Date(a.frontmatter.date).getTime()
+            )
+        })
+    },
+})
