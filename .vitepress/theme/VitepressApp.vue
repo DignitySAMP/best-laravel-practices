7e3a8e853ffb2a9add6e9a69668e7a7dcfb85238
Davy <davy@datalink.be>
2025-12-10 13:24:29 +0100

    🕰️ wip: add cookie banner
    => transition to slide banner up or down
    => ability to close the banner or 'manage preferences'

--- .vitepress/theme/VitepressApp.vue
+++ .vitepress/theme/VitepressApp.vue
@@ -1,12 +1,34 @@
 <template>
-    <AppLayoutError v-if="page.isNotFound"/>
-    <AppLayoutIndex v-else-if="frontmatter.layout === 'home'" />
-    <AppLayoutResource v-else-if="frontmatter.layout === 'resources'" />
-    <AppLayoutDefault v-else>
-        <Content />
-    </AppLayoutDefault>
+    <div class="relative h-screen">
+        <AppLayoutError v-if="page.isNotFound"/>
+        <AppLayoutIndex v-else-if="frontmatter.layout === 'home'" />
+        <AppLayoutResource v-else-if="frontmatter.layout === 'resources'" />
+        <AppLayoutDefault v-else>
+            <Content />
+        </AppLayoutDefault>
+
+
+        <Transition
+            enter-active-class="transition transform duration-300"
+            enter-from-class="translate-y-full opacity-0"
+            enter-to-class="translate-y-0 opacity-100"
+            
+            leave-active-class="transition transform duration-300"
+            leave-from-class="translate-y-0 opacity-100"
+            leave-to-class="translate-y-full opacity-0"
+        >
+            <AppCookie v-if="showCookieBanner" v-model="showCookieBanner"/>
+        </Transition>
+    </div>
 </template>
 <script setup lang="ts">
 import { useData, Content } from 'vitepress'
 const { page, frontmatter } = useData()
+
+const showCookieBanner = ref<boolean>(false);
+onMounted(() => {
+    setTimeout(() => {
+        showCookieBanner.value = true;
+    }, 500)
+})
 </script>
---
+++ .vitepress/theme/components/AppCookie.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="absolute bottom-1 bg-white w-full border-t-4 border-red-600 px-4 py-2 flex flex-col">
+        <div class="flex items-center justify-between">
+            <div class="flex flex-col">
+                <span class="font-bold text-lg">
+                    This website uses cookies.
+                </span>
+                <span>
+                    We only use <span class="font-bold">mandatory</span> cookies to store your <span class="font-bold">language</span> and  <span class="font-bold">dark mode preferences</span>. Additionally, we store your cookie preference (true or false) in your browser's local storage.
+                    These cookies are mandatory for the website to function. We do not track or store anything else.
+                </span>
+            </div>
+            <div class="flex gap-2 px-6 w-fit">
+                <AppFormButton @click="managePreferences = true;" class="w-full flex" theme="primary">
+                    Show preferences
+                </AppFormButton>
+            </div>
+            <span @click="showCookieBanner = false">
+                x
+            </span>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+
+    const showCookieBanner = defineModel();
+    const managePreferences = ref<boolean>(false);
+</script>
