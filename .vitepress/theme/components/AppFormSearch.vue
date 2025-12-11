3fe88de7a9502d0f2c67851f69887758009153a7
Davy <davy@datalink.be>
2025-12-11 08:07:55 +0100

    ♻️ refactor(style): remove manual line-clamp class

--- .vitepress/theme/components/AppFormSearch.vue
+++ .vitepress/theme/components/AppFormSearch.vue
@@ -184,12 +184,3 @@
         }
     })
 </script>
-  
-<style scoped>
-  .line-clamp-2 {
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-  }
-</style>
